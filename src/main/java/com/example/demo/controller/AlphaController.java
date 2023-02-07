fa037ad8e5fc41150c2e15da464cc8c3835408e7
yalger <yalger@qq.com>
2023-02-07 13:34:01 +0800

    会话管理

--- src/main/java/com/example/demo/controller/AlphaController.java
+++ src/main/java/com/example/demo/controller/AlphaController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.AlphaService;
+import com.example.demo.util.DemoUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -135,5 +138,45 @@ public class AlphaController {
         list.add(employee);
 
         return list;
+    }
+
+    // Cookie示例
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        // 创建Cookie
+        Cookie cookie = new Cookie("code", DemoUtil.generateUUID());
+        // 设置Cookie生效的范围
+        cookie.setPath("");
+        // 设置Cookie的生存时间
+        cookie.setMaxAge(60 * 10);  // 10分钟
+        // 发送Cookie
+        response.addCookie(cookie);
+
+        return "set cookie";
+    }
+
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        System.out.println(code);
+        return "get cookie";
+    }
+
+    // Session示例
+    @RequestMapping(path = "/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("id", 1);
+        session.setAttribute("name", "张三");
+        return "set session";
+    }
+
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
     }
 }
