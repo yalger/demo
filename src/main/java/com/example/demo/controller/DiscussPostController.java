0bb115b8f9c480341371deee57ce3f82b42e47f0
yalger <yalger@qq.com>
2023-02-08 23:23:14 +0800

    帖子详情

--- src/main/java/com/example/demo/controller/DiscussPostController.java
+++ src/main/java/com/example/demo/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.entity.DiscussPost;
 import com.example.demo.entity.User;
 import com.example.demo.service.DiscussPostService;
+import com.example.demo.service.UserService;
 import com.example.demo.util.DemoUtil;
 import com.example.demo.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +26,9 @@ public class DiscussPostController {
     @Autowired
     private HostHolder hostHolder;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
     public String addDiscussPost(String title, String content) {
@@ -38,6 +44,18 @@ public class DiscussPostController {
         discussPostService.addDiscussPost(post);
 
         return DemoUtil.getJSONString(0, "发布成功！");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 查询帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        // 作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
     }
 
 }
