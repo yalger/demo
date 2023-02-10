dcf8b64c1bee621f7bdb1e809ebdf5bf7d898a57
yalger <yalger@qq.com>
2023-02-10 20:41:30 +0800

    删除私信

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -142,4 +142,12 @@ public class MessageController {
         return DemoUtil.getJSONString(0);
     }
 
+    // 删除私信
+    @RequestMapping(path = "/letter/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public String deleteLetter(int id) {
+        messageService.deleteMessage(id);
+        return DemoUtil.getJSONString(0);
+    }
+
 }
